aba8fc2a04317c1ef1d00e100363e60ade0a39e6
hanifrosidi <hanifrosidi@gmail.com>
2026-05-20 07:41:39 +0700

    add build_push_image_karsajobs_ui.sh

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+build_image() {
+    echo "build image starting.................."
+    docker build -t hanifrosidi/karsajobs-ui:latest
+}
+
+login_docker_repo() {
+    echo "login docker hub starting................."
+    docker login
+}
+
+push_image() {
+    echo "push image starting........................"
+    docker tag karsajobs-ui:latest hanifrosidi/karsajobs-ui:latest
+    docker push hanifrosidi/karsajobs-ui:latest
+}
+
+finish_program() {
+    echo "==========================================================="
+    echo "==================Program Finished========================="
+    echo "Clear screen automated....................................."
+    sleep 3
+    clear
+}
+
+main() {
+    while true
+    do
+        echo "==========================================================="
+        echo "==============Karsa Jobs UI Image Management==============="
+        echo "1. Build Docker Image"
+        echo "2. Login To Docker Hub"
+        echo "3. Push Docker Image To Docker Hub"
+        echo "==========================================================="
+        echo "Pilih menu 1-3, dan q untuk keluar program"
+
+        read -p "Masukkan pilihan menu: " menu
+        echo "$menu"
+        
+
+        if [ "$menu" = "1" ]; then
+            build_image
+        elif [ "$menu" = "2" ]; then
+            login_docker_repo
+        elif [ "$menu" = "3" ]; then
+            push_image
+        elif [ "$menu" = "q" ]; then
+            finish_program
+            break
+        else
+            echo "Pilihan tidak valid"
+            sleep 1
+            clear
+        fi
+    done
+
+}
+
+main
